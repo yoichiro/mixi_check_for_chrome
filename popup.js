3a6e69d09dcff5c03ccb4ee07dd3b5290928e786
Yoichiro Tanaka <yoichiro@eisbahn.jp>
2011-11-07 16:29:31 +0900

    Added the code to close the popup window.

--- popup.js
+++ popup.js
@@ -4,12 +4,14 @@ var Mixi = function() {
 
 Mixi.prototype = {
     initialize: function() {
+        this.assignEventHandler();
     },
     render: function() {
         chrome.tabs.getSelected(null, function(tab) {
             var url = "http://mixi.jp/share.pl?u="
                 + encodeURIComponent(tab.url)
-                + "&k=f45808baee7c83d97158c5dd83b5f67665add179";
+                + "&k=f45808baee7c83d97158c5dd83b5f67665add179"
+                + "&m=iframe";
             var iframe = document.createElement("iframe");
             iframe.src = url;
             iframe.scrolling = "no";
@@ -19,6 +21,14 @@ Mixi.prototype = {
                 });
             }.bind(this);
             $("target").appendChild(iframe);
+        }.bind(this));
+    },
+    assignEventHandler: function() {
+        window.addEventListener("message", function(evt) {
+            if (evt.origin === "http://mixi.jp"
+                && evt.data === "close") {
+                window.close();
+            }
         }.bind(this));
     }
 };
